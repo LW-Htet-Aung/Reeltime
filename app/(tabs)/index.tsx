3159ed4b6df059478f10d66ba78516d16c74157c
LW-Htet-Aung <lwhtetaung80@gmail.com>
2025-03-20 22:18:13 +0630

    reeltime - bottom navigation bar added

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className="items-center justify-center flex-1">
+      <Text className="text-5xl text-accent">Welcome</Text>
+    </View>
+  );
+}
