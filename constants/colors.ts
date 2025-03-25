3b32f8c77edbbd4498fb0c8c831255dac499ffc0
LW-Htet-Aung <lwhtetaung80@gmail.com>
2025-03-25 23:24:27 +0630

    reeltime  - update ui and packages

---
+++ constants/colors.ts
@@ -0,0 +1,3 @@
+export const colors = {
+  primary: "#a8b5ff",
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
         primary: "#030014",
-        accent: "#ab8bff",
         secondary: "#151312",
         light: {
-          100: "#d6c6ff",
-          200: "#a8b5db",
-          300: "#9ca4ab",
+          100: "#D6C7FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
         },
         dark: {
-          100: "#221f3d",
-          200: "#0f0d23",
+          100: "#221F3D",
+          200: "#0F0D23",
         },
+        accent: "#AB8BFF",
       },
     },
   },
